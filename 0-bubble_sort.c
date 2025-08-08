8eec38fd196f087e0da85b255a44c32f9176c172
salagreg <greg.sala@icloud.com>
2025-08-08 19:46:52 +0200

    Ajout de fichier vers main

---
+++ 0-bubble_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+/**
+ * bubble_sort - Trie un tableau d'entiers dans l'ordre croissant
+ * @array: Pointeur vers le tableau d'entiers a trier
+ * @size: Taille du tableau
+ * Return: neant
+ */
+void bubble_sort(int *array, size_t size)
+{
+  size_t i, j;
+  int temp;
+
+  /* Verifie tableau valide et s'il contient au moins 2 elements */
+  if (array == NULL || size < 2)
+    return;
+
+  /* Boucle exterieure : nombre total de passes */
+  for (i = 0; i < size - 1; i++)
+  {
+    /* Boucle intérieure : compare les elements non tries */
+    for (j = 0; j < size - i - 1; j++)
+    {
+      /* Si deux elements sont dans le mauvais ordre, on les echange */
+      if (array[j] > array[j + 1])
+      {
+        /* Echange des deux elements */
+        temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+        
+        /* Affiche l'etat du tableau apres chaque echange */
+        print_array(array, size);
+      }
+    }
+  }
+}
