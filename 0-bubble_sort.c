84f1ec5d1109d07601987f1a3082419c39beac3f
Lucas-blancaws <lblancportier@gmail.com>
2025-08-08 20:06:58 +0200

    betty 0_3

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,36 +1,36 @@
 #include "sort.h"
 /**
- * bubble_sort - Trie un tableau d'entiers dans l'ordre croissant
- * @array: Pointeur vers le tableau d'entiers a trier
- * @size: Taille du tableau
- * Return: neant
- */
+* bubble_sort - Trie un tableau d'entiers dans l'ordre croissant
+* @array: Pointeur vers le tableau d'entiers a trier
+* @size: Taille du tableau
+* Return: neant
+*/
 void bubble_sort(int *array, size_t size)
 {
-  size_t i, j;
-  int temp;
+size_t i, j;
+int temp;
 
-  /* Verifie tableau valide et s'il contient au moins 2 elements */
-  if (array == NULL || size < 2)
-    return;
+/* Verifie tableau valide et s'il contient au moins 2 elements */
+if (array == NULL || size < 2)
+	return;
 
-  /* Boucle exterieure : nombre total de passes */
-  for (i = 0; i < size - 1; i++)
-  {
-    /* Boucle intérieure : compare les elements non tries */
-    for (j = 0; j < size - i - 1; j++)
-    {
-      /* Si deux elements sont dans le mauvais ordre, on les echange */
-      if (array[j] > array[j + 1])
-      {
-        /* Echange des deux elements */
-        temp = array[j];
-        array[j] = array[j + 1];
-        array[j + 1] = temp;
-        
-        /* Affiche l'etat du tableau apres chaque echange */
-        print_array(array, size);
-      }
-    }
-  }
+/* Boucle exterieure : nombre total de passes */
+for (i = 0; i < size - 1; i++)
+{
+	/* Boucle intérieure : compare les elements non tries */
+	for (j = 0; j < size - i - 1; j++)
+	{
+	/* Si deux elements sont dans le mauvais ordre, on les echange */
+	if (array[j] > array[j + 1])
+	{
+		/* Echange des deux elements */
+		temp = array[j];
+		array[j] = array[j + 1];
+		array[j + 1] = temp;
+
+		/* Affiche l'etat du tableau apres chaque echange */
+		print_array(array, size);
+	}
+	}
+}
 }
--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,19 +1,19 @@
 #include "sort.h"
 /**
- * partition - Fonction de partitionnement pour le tri rapide
- * @array: Tableau d'entiers a trier
- * @low: Indice de debut du sous-tableau
- * @high: Indice de fin du sous-tableau (pivot)
- * @size: Taille totale du tableau (pour print_array)
- * Return: L’indice final du pivot apres le partitionnement
- */
+* partition - Fonction de partitionnement pour le tri rapide
+* @array: Tableau d'entiers a trier
+* @low: Indice de debut du sous-tableau
+* @high: Indice de fin du sous-tableau (pivot)
+* @size: Taille totale du tableau (pour print_array)
+* Return: L’indice final du pivot apres le partitionnement
+*/
 int partition(int *array, int low, int high, int size)
 {
-  int pivot = array[high];
-  int i = low, j, temp;
+int pivot = array[high];
+int i = low, j, temp;
 
-  for (j = low; j < high; j++)
-  { 
+for (j = low; j < high; j++)
+{
 	  if (array[j] <= pivot)
 	  {
 		  if (i < j)
@@ -25,25 +25,25 @@ int partition(int *array, int low, int high, int size)
 		  }
 		  i++;
 	  }
-  }
-  if (i < high)
-  {
+}
+if (i < high)
+{
 	  temp = array[i];
 	  array[i] = array[high];
 	  array[high] = temp;
 	  print_array(array, size);
-  }
-  return (i);
+}
+return (i);
 }
 
 /**
- * quick_sort_recursive - Fonction recursive de tri rapide
- * @array: Tableau a trier
- * @low: Indice de debut du sous-tableau
- * @high: Indice de fin du sous-tableau
- * @size: Taille totale du tableau (pour print_array)
- * Return: neant
- */
+* quick_sort_recursive - Fonction recursive de tri rapide
+* @array: Tableau a trier
+* @low: Indice de debut du sous-tableau
+* @high: Indice de fin du sous-tableau
+* @size: Taille totale du tableau (pour print_array)
+* Return: neant
+*/
 void quick_sort_recursive(int *array, int low, int high, int size)
 {
 	int pivot;
@@ -57,15 +57,15 @@ void quick_sort_recursive(int *array, int low, int high, int size)
 }
 
 /**
- * quick_sort - Fonction principale du tri rapide
- * @array: Tableau a trier
- * @size: Taille du tableau
- * Return: neant
- */
+* quick_sort - Fonction principale du tri rapide
+* @array: Tableau a trier
+* @size: Taille du tableau
+* Return: neant
+*/
 void quick_sort(int *array, size_t size)
 {
 	if (array == NULL || size < 2)
 		return;
-	
-  quick_sort_recursive(array, 0, size - 1, size);
+
+quick_sort_recursive(array, 0, size - 1, size);
 }
